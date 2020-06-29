a03262ab79fe37f16d81bd1340aa916bce61c0db
Albert Folch <albertfolchg@gmail.com>
2020-06-29 10:23:28 +0200

    Update index.tsx
    Added exported TesfyContext

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import useTesfy from './useTesfy';
 import useExperiment from './useExperiment';
 import useFeature from './useFeature';
+import TesfyContext from './TesfyContext';
 import TesfyProvider, { createInstance } from './TesfyProvider';
 import Experiment from './Experiment';
 import Variation from './Variation';
@@ -11,6 +12,7 @@ export {
   useTesfy,
   useExperiment,
   useFeature,
+  TesfyContext,
   TesfyProvider,
   Experiment,
   Variation,
